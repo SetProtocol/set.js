1388e1c3ae739ccb8dbc269ff654cc77e84f3473
Alexander Soong <alexsoong91@gmail.com>
2020-08-01 23:29:46 -0700

    Add additional ContractWrapper interfaces and specs

---
+++ src/wrappers/set-protocol-v2/ControllerWrapper.ts
@@ -0,0 +1,111 @@
+/*
+  Copyright 2020 Set Labs Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+'use strict';
+
+import { ContractTransaction } from 'ethers';
+import { Provider, JsonRpcProvider } from 'ethers/providers';
+import { Address, Position } from 'set-protocol-v2/utils/types';
+
+import { ContractWrapper } from './ContractWrapper';
+
+/**
+ * @title  ControllerWrapper
+ * @author Set Protocol
+ *
+ * The Controller wrapper handles all functions on the Controller smart contract.
+ *
+ */
+export class ControllerWrapper {
+  private provider: Provider;
+  private contracts: ContractWrapper;
+
+  private controllerAddress: Address;
+
+  public constructor(provider: Provider, controllerAddress: Address) {
+    this.provider = provider;
+    this.contracts = new ContractWrapper(provider);
+    this.controllerAddress = controllerAddress;
+  }
+
+  /**
+   * Returns the array of Factory contracts
+   *
+   * @param controllerAddress  Controller address to get list of Factories for
+   * @return                   Array of Factory addresses
+   */
+  public async getFactories(
+    callerAddress?: Address,
+  ): Promise<Address[]> {
+    const controller = await this.contracts.loadControllerContractAsync(
+      this.controllerAddress,
+      (this.provider as JsonRpcProvider).getSigner(callerAddress)
+    );
+
+    return controller.getFactories();
+  }
+
+  /**
+   * Returns the array of Module contracts
+   *
+   * @param controllerAddress  Controller address to get list of Modules for
+   * @return                   Array of Module addresses
+   */
+  public async getModules(
+    callerAddress?: Address,
+  ): Promise<Address[]> {
+    const controller = await this.contracts.loadControllerContractAsync(
+      this.controllerAddress,
+      (this.provider as JsonRpcProvider).getSigner(callerAddress)
+    );
+
+    return controller.getModules();
+  }
+
+  /**
+   * Returns the array of Resource contracts
+   *
+   * @param controllerAddress  Controller address to get list of Resources for
+   * @return                   Array of Resouce addresses
+   */
+  public async getResources(
+    callerAddress?: Address,
+  ): Promise<Address[]> {
+    const controller = await this.contracts.loadControllerContractAsync(
+      this.controllerAddress,
+      (this.provider as JsonRpcProvider).getSigner(callerAddress)
+    );
+
+    return controller.getResources();
+  }
+
+  /**
+   * Returns the array of SetToken contracts
+   *
+   * @param controllerAddress  Controller address to get list of Sets for
+   * @return                   Array of SetToken addresses
+   */
+  public async getSets(
+    callerAddress?: Address,
+  ): Promise<Address[]> {
+    const controller = await this.contracts.loadControllerContractAsync(
+      this.controllerAddress,
+      (this.provider as JsonRpcProvider).getSigner(callerAddress)
+    );
+
+    return controller.getSets();
+  }
+}
---
+++ test/wrappers/set-protocol-v2/ControllerWrapper.spec.ts
@@ -0,0 +1,178 @@
+import { ethers } from 'ethers';
+import { BigNumber } from 'ethers/utils';
+
+import { Address } from 'set-protocol-v2/utils/types';
+import { Blockchain, ether } from 'set-protocol-v2/dist/utils/common';
+import DeployHelper from 'set-protocol-v2/dist/utils/deploys';
+import { ControllerWrapper } from '../../../src/wrappers/set-protocol-v2/ControllerWrapper';
+import { Controller } from 'set-protocol-v2/dist/typechain/Controller';
+
+const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545');
+import { expect } from './utils/chai';
+
+const blockchain = new Blockchain(provider);
+
+
+describe('ControllerWrapper', () => {
+  let owner: Address;
+  let manager: Address;
+  let mockSetTokenFactory: Address;
+  let mockModule: Address;
+  let mockSetTokenAddress: Address;
+  let mockPriceOracleAddress: Address;
+  let functionCaller: Address;
+
+  let controller: Controller;
+  let controllerWrapper: ControllerWrapper;
+
+  let deployer: DeployHelper;
+
+  beforeEach(async () => {
+    [
+      owner,
+      manager,
+      mockSetTokenFactory,
+      mockModule,
+      mockSetTokenAddress,
+      mockPriceOracleAddress,
+      functionCaller,
+    ] = await provider.listAccounts();
+
+    deployer = new DeployHelper(provider.getSigner(owner));
+    controller = await deployer.core.deployController(owner);
+
+    controllerWrapper = new ControllerWrapper(provider, controller.address);
+  });
+
+  beforeEach(async () => {
+    await blockchain.saveSnapshotAsync();
+  });
+
+  afterEach(async () => {
+    await blockchain.revertAsync();
+  });
+
+  describe('#getFactories', () => {
+    let subjectCaller: Address;
+
+    beforeEach(async () => {
+      subjectCaller = functionCaller;
+    });
+
+    async function subject(): Promise<Address[]> {
+      return controllerWrapper.getFactories(subjectCaller);
+    }
+
+    it('should return empty array', async () => {
+      const factoryAddresses = await subject();
+
+      expect(factoryAddresses.length).to.eq(0);
+    });
+
+    describe('when there is a deployed SetToken', () => {
+      beforeEach(async () => {
+        await controller.addFactory(mockSetTokenFactory);
+      });
+
+      it('returns the Factory address as part of factories', async () => {
+        const factoryAddresses = await subject();
+
+        expect(factoryAddresses[0]).to.eq(mockSetTokenFactory);
+      });
+    });
+  });
+
+  describe('#getModules', () => {
+    let subjectCaller: Address;
+
+    beforeEach(async () => {
+      subjectCaller = functionCaller;
+    });
+
+    async function subject(): Promise<Address[]> {
+      return controllerWrapper.getModules(subjectCaller);
+    }
+
+    it('should return empty array', async () => {
+      const moduleAddresses = await subject();
+
+      expect(moduleAddresses.length).to.eq(0);
+    });
+
+    describe('when there is a deployed SetToken', () => {
+      beforeEach(async () => {
+        await controller.addModule(mockModule);
+      });
+
+      it('returns the Module address as part of modules', async () => {
+        const modules = await subject();
+
+        expect(modules[0]).to.eq(mockModule);
+      });
+    });
+  });
+
+  describe('#getResources', () => {
+    let subjectCaller: Address;
+
+    beforeEach(async () => {
+      subjectCaller = functionCaller;
+    });
+
+    async function subject(): Promise<Address[]> {
+      return controllerWrapper.getResources(subjectCaller);
+    }
+
+    it('should return empty array', async () => {
+      const resources = await subject();
+
+      expect(resources.length).to.eq(0);
+    });
+
+    describe('when there is a resource added', () => {
+      beforeEach(async () => {
+        const resourceId: BigNumber = new BigNumber(0);
+        await controller.addResource(mockPriceOracleAddress, resourceId);
+      });
+
+      it('returns the resource as part of resources', async () => {
+        const resources = await subject();
+
+        expect(resources[0]).to.eq(mockPriceOracleAddress);
+      });
+    });
+  });
+
+  describe('#getSets', () => {
+    let subjectCaller: Address;
+
+    beforeEach(async () => {
+      subjectCaller = functionCaller;
+    });
+
+    async function subject(): Promise<Address[]> {
+      return controllerWrapper.getSets(subjectCaller);
+    }
+
+    it('should return empty array', async () => {
+      const setAddresses = await subject();
+
+      expect(setAddresses.length).to.eq(0);
+    });
+
+    describe('when there is a deployed SetToken', () => {
+      beforeEach(async () => {
+        await controller.addFactory(mockSetTokenFactory);
+
+        controller = controller.connect(provider.getSigner(mockSetTokenFactory));
+        await controller.addSet(mockSetTokenAddress);
+      });
+
+      it('returns the SetToken as part of sets', async () => {
+        const setAddresses = await subject();
+
+        expect(setAddresses[0]).to.eq(mockSetTokenAddress);
+      });
+    });
+  });
+});
