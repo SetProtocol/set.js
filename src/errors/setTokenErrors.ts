0ff11e697b0fe33ad23fcbb1a98d470eee94b147
controtie <dylan.ad.tran@gmail.com>
2020-08-05 22:19:14 -0700

    add set token, schema errors

--- src/errors/setTokenErrors.ts
+++ src/errors/setTokenErrors.ts
@@ -1,4 +1,6 @@
 import { Address } from 'set-protocol-v2/utils/types';
 
-export const GET_CONTROLLER_ASSERTION_ERROR = (setAddress: Address) =>
-  `Provided address ${setAddress} is not a valid address.`;
+export const setTokenErrors = {
+  GET_CONTROLLER_ASSERTION_ERROR: (setAddress: Address) =>
+    `Provided address ${setAddress} is not a valid address.`,
+};
