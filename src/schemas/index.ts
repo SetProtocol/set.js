5f1252d5794647f0353f6783250b28d1725237b3
controtie <dylan.ad.tran@gmail.com>
2020-08-05 22:22:41 -0700

    export common schemas

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -16,18 +16,4 @@
 
 'use strict';
 
-import {
-  addressSchema,
-  bytes32Schema,
-  bytesSchema,
-  numberSchema,
-  wholeNumberSchema,
-} from './basicTypeSchemas';
-
-export const schemas = {
-  addressSchema,
-  numberSchema,
-  bytesSchema,
-  bytes32Schema,
-  wholeNumberSchema,
-};
+export { commonSchemas } from './commonSchemas';
