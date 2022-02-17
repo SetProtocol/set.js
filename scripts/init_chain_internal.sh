27c7fd2051592ad2b3635462bcf2141fec8ed4a4
cgewecke <christophergewecke@gmail.com>
2022-02-17 09:15:25 -0800

    Upgrade ganache to V7

--- scripts/init_chain_internal.sh
+++ scripts/init_chain_internal.sh
@@ -4,4 +4,9 @@ SET_PROTOCOL_JS=`pwd`
 
 rm -rf blockchain && cp -r snapshots/0x-Kyber-Compound blockchain
 
-ganache-cli --networkId 50 --accounts 20 -l 20000000 -e 10000000000 --mnemonic 'concert load couple harbor equip island argue ramp clarify fence smart topic'
+ganache \
+  --chain.networkId 50 \
+  --miner.blockGasLimit 20000000 \
+  --wallet.totalAccounts 20  \
+  --wallet.defaultBalance 10000000000 \
+  --wallet.mnemonic 'concert load couple harbor equip island argue ramp clarify fence smart topic'
