c299a43c286ded9ea3db1ff7c0ff07e856415a7b
Dylan Tran <dylan.ad.tran@gmail.com>
2022-05-04 16:50:56 -0700

    add null address constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,5 @@
+import { Address } from '@setprotocol/set-protocol-v2/utils/types';
+
 export const DEFAULT_GAS_LIMIT: number = 12500000; // default of 12.5 million gas
 export const DEFAULT_GAS_PRICE: number = 6000000000; // 6 gwei
+export const NULL_ADDRESS: Address = '0x0000000000000000000000000000000000000000';
